ac974d1deda23ff3d5fc9c99ec14246b4194c163
Lab User <LabUser@labs.salina.k-state.edu>
2018-09-18 11:05:02 -0500

    Added an app.js to manage connection to MongoDB

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://locallibrary:Sc0ttc4rd@ds149682.mlab.com:49682/local_library_mjp';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
